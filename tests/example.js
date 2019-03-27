b4fd423439de0cf735793222585bbeba8a0d1173
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-03-27 13:49:58 -0700

    Using Mocha as test runner

--- tests/example.js
+++ tests/example.js
@@ -1,22 +1,10 @@
-module.exports = {
-  'can run' : function (client) {
-    // client
-    //   .url('http://www.google.com')
-    //   .waitForElementVisible('body', 1000)
-    //   .assert.title('Google')
-    //   .assert.visible('input[type=text]')
-    //   .setValue('input[type=text]', 'rembrandt van rijn')
-    //   .waitForElementVisible('button[name=btnG]', 1000)
-    //   .click('button[name=btnG]')
-    //   .pause(1000)
-    //   .assert.containsText('ol#rso li:first-child',
-    //     'Rembrandt - Wikipedia')
-    //   .end();
-    client
+describe('Can run', function() {
+  it('load viewing sample', function(browser) {
+    browser
       // .url('http://192.168.1.72:3000/samples/viewing/viewing')
       .url('http://localhost:3000/samples/viewing/viewing')
       .waitForElementVisible('body', 1000)
       .pause(30000)
       .end();
-  }
-};
+  })
+})

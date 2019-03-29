c745d1730ef53f52d53892526fba21608fa4b42e
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-03-28 17:09:09 -0700

    refactor encrypted password to use pageObject

---
+++ tests/nightwatch/pageObjects/passwordModal.js
@@ -0,0 +1,19 @@
+module.exports = {
+  elements: {
+    modal: {
+      selector: '[data-element="passwordModal"]'
+    },
+    input: {
+      selector: 'input[type=password]'
+    },
+    submitBtn: {
+      selector: '[data-element="passwordSubmitButton"]'
+    },
+    cancelBtn: {
+      selector: '[data-element="passwordCancelButton"]'
+    },
+    wrongPasswordDiv: {
+      selector: '.incorrect-password'
+    }
+  }
+}
--- tests/nightwatch/release/loadPDF.js
+++ tests/nightwatch/release/loadPDF.js
@@ -65,44 +65,48 @@ describe('Load PDF', function() {
     })
 
     describe('Encrypted PDF', function() {
+      let passwordModal;
+
       beforeEach(function(client, done) {
-        client
-          .readerControl('loadDocument', '/samples/files/encrypted-foobar12.pdf')
-          .waitForElementVisible('[data-element="passwordModal"]', 5000, () => done());
+        passwordModal = client.page.passwordModal();
+
+        client.readerControl('loadDocument', '/samples/files/encrypted-foobar12.pdf');
+        passwordModal.waitForElementVisible('@modal', 5000, () => done());
       })
 
       it('show password modal', function() {})
 
       it('load successfully after the correct password is submitted', function(client) {
+        passwordModal
+          .setValue('@input', 'foobar12')
+          .click('@submitBtn');
         client
-          .setValue('input[type=password]', 'foobar12')
-          .click('[data-element="passwordSubmitButton"]')
-          .waitForWVEvent('pageComplete')
+          .waitForWVEvent('pageComplete');
       })
 
-      it('show wrong password information after a wrong password is submitted', function(client) {
-        client
-          .setValue('input[type=password]', 'this must not be the correct password')
-          .click('[data-element="passwordSubmitButton"]')
-          .assert.cssClassPresent('input[type=password]', 'wrong')
-          .waitForElementVisible('.incorrect-password', 5000);
+      it('show wrong password information after a wrong password is submitted', function() {
+        passwordModal
+          .setValue('@input', 'this cannot be the correct password ^_^')
+          .click('@submitBtn')
+          .assert.cssClassPresent('@input', 'wrong')
+          .waitForElementVisible('@wrongPasswordDiv', 5000)
       })
 
-      it('remain visible after cancel button is clicked', function(client) {
-        client
-          .click('[data-element="passwordCancelButton"]')
-          .expect.element('[data-element="passwordModal"]').to.be.present;
+      it('remain visible after cancel button is clicked', function() {
+        passwordModal
+          .click('@cancelBtn')
+          .expect.element('@modal').to.be.present;
       })
     })
 
-    describe('PDF with JavaScript', function() {
-      it.only('load javascript.pdf', function(client) {
-        client
-          .readerControl('loadDocument', '/samples/files/javascript.pdf')
-          .waitForWVEvent('annotationsLoaded')
-          .pause(5000)
-      })
-    })
+    // describe('PDF with JavaScript', function() {
+    //   it.only('load javascript.pdf', function(client) {
+    //     client
+    //       .readerControl('loadDocument', '/samples/files/javascript.pdf')
+    //       .waitForWVEvent('annotationsLoaded')
+    //       .pause(5000)
+    //   })
+    // })
   });
 
   describe('With Server', function() {

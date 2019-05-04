9f7cc03ce5d027773ae835c02ac153968ee65b23
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-05-03 17:55:24 -0700

    finish sample tests

--- tests/nightwatch/release/sample/text-search.test.js
+++ tests/nightwatch/release/sample/text-search.test.js
@@ -1,5 +1,5 @@
 describe('Text Search', function() {
-  it.only('search text using strings and regex in a PDF document', function(client) {
+  it('search text using strings and regex in a PDF document', function(client) {
     client
       .loadSample('full-apis/TextSearchTest')
       .waitForConsoleLog('Is the owner\'s name: Ben Franklin?');

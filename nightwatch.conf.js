c6c35b186b0f144847322b1efc9449f66765448f
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-05-07 15:30:19 -0700

    remove conf.js

--- nightwatch.conf.js
+++
@@ -1,8 +0,0 @@
-const chromedriver = require('chromedriver');
-const geckodriver = require('geckodriver');
-
-module.exports = (function(settings) {
-  settings.test_workers = false;
-  settings.webdriver.server_path = chromedriver.path;
-  return settings;
-})(require('./nightwatch.json'));

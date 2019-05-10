0e64c308d0896a6eddf08cd69548fb8e324f7f13
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-05-10 14:31:36 -0700

    able to run on bs, prepare for refactorring the config files

--- nightwatch.bs.conf.js
+++ nightwatch.bs.conf.js
@@ -8,16 +8,15 @@ module.exports = (function(settings) {
   settings['test_settings'] = {
     'default': {
       'desiredCapabilities': {
-        'browserstack.user': 'zhijiezhang1',
-        'browserstack.key': 'QKPysHGgp6Ykq92rT9Hf',
-        'browserstack.local': true,
-        'browserstack.debug': true,
-        'browserstack.video': true,
-        'os': 'Windows',
-        'os_version': '10',
-        'browserName': 'Chrome',
-        'browser_version': '74.0',
-        'resolution': '1280x1024',
+        'os' : 'Windows',
+        'os_version' : '10',
+        'browserName' : 'Chrome',
+        'browser_version' : '74.0',
+        'resolution' : '1920x1080',
+        'browserstack.local' : 'true',
+        'browserstack.selenium_version' : '3.14.0',
+        'browserstack.user' : 'zhijiezhang1',
+        'browserstack.key' : 'QKPysHGgp6Ykq92rT9Hf',
         'loggingPrefs': { 
           'browser': 'ALL'
         }

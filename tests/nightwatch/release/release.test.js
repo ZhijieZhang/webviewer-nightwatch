e16fb5cbcc15e860e66ec93af0cc841dabe5deb1
Zhijie Zhang <zhijiezhang0124@gmail.com>
2019-05-13 12:07:25 -0700

    test the realtime-collaboration sample

--- tests/nightwatch/release/release.test.js
+++ tests/nightwatch/release/release.test.js
@@ -7,56 +7,32 @@ describe('WebViewer Release Test', function() {
     client.end(() => done());
   });
 
-  describe('PDF', function() {
-    beforeEach(function(client, done) {
-      client 
-        .loadSample('viewing/viewing')
-        .setUrlWithOptions({ 
-          pdftronServer: '' 
-        })
-        .waitForWVEvent('pageComplete', done);
-    });
+  // describe('PDF', function() {
+  //   beforeEach(function(client, done) {
+  //     client 
+  //       .loadSample('viewing/viewing')
+  //       .setUrlWithOptions({ 
+  //         pdftronServer: '' 
+  //       })
+  //       .waitForWVEvent('pageComplete', done);
+  //   });
 
-    it.only('click a nested outline in a file', function(client) {
-      // expand all the outlines by clicking on all the arrows
-      // and then choose a nested outline to click
-      client
-        .readerControl('loadDocument', '/samples/files/outlines-nested.pdf')
-        .waitForWVEvent('pageComplete')
-        .readerControl('openElement', 'outlinesPanel')
-        // wait for the left panel to be fully opened
-        .pause(500)
-        .elements('css selector', '.Outline .arrow', function({ value: arrows }) {
-          arrows.forEach(function(arrow) {
-            const id = Object.values(arrow)[0];
-            client.elementIdClick(id);
-          });
-        })
-        .elements('css selector', '.Outline', function ({ value: outlines }) {
-          client
-            .elementIdClick(Object.values(outlines[5])[0])
-            .readerControl('getCurrentPageNumber', function (currentPage) {
-              assert.equal(currentPage, 6);
-            });
-        });
-    });
-
-    // importTests([   
-    //   './PDF/linearized.test.js',
-    //   './PDF/non-linearized.test.js',
-    //   './PDF/encryption.test.js',
-    //   './PDF/javascript.test.js',
-    //   './PDF/substituted-font.test.js',
-    //   './PDF/invalid-key.test.js',
-    //   ['./common/text-selection.test.js'],
-    //   ['./common/print.test.js'],
-    //   ['./common/load-document.test.js', 'sample.pdf', 'sample-annotated.pdf'],
-    //   ['./common/annotation.test.js', 'sample.pdf'],
-    //   ['./common/form.test.js', 'form.pdf'],
-    //   ['./common/outline.test.js', 'outlines-nested.pdf'],
-    //   ['./common/layout-mode.test.js', 'webviewer-demo-annotated.pdf']
-    // ]);
-  });
+  //   importTests([   
+  //     './PDF/linearized.test.js',
+  //     './PDF/non-linearized.test.js',
+  //     './PDF/encryption.test.js',
+  //     './PDF/javascript.test.js',
+  //     './PDF/substituted-font.test.js',
+  //     './PDF/invalid-key.test.js',
+  //     ['./common/text-selection.test.js'],
+  //     ['./common/print.test.js'],
+  //     ['./common/load-document.test.js', 'sample.pdf', 'sample-annotated.pdf'],
+  //     ['./common/annotation.test.js', 'sample.pdf'],
+  //     ['./common/form.test.js', 'form.pdf'],
+  //     ['./common/outline.test.js', 'outlines-nested.pdf'],
+  //     ['./common/layout-mode.test.js', 'webviewer-demo-annotated.pdf']
+  //   ]);
+  // });
 
   // describe.skip('XOD', function() {
   //   beforeEach(function(client, done) {
@@ -80,55 +56,55 @@ describe('WebViewer Release Test', function() {
   //   ]);
   // });
 
-  // describe.skip('Samples', function() {
-  //   importTests([
-  //     './sample/legacy-viewing.test.js',
-  //     './sample/users-and-permissions.test.js',
-  //     './sample/realtime-collaboration.test.js',
-  //     './sample/custom-annotations.test.js',
-  //     './sample/ui-customization.test.js',
-  //     './sample/form-customization.test.js',
-  //     './sample/i18n.test.js',
-  //     './sample/page-operations.test.js',
-  //     './sample/layer-separation.test.js',
-  //     './sample/color-separation.test.js',
-  //     './sample/XOD.test.js',
-  //     './sample/text-position.test.js',
-  //     './sample/redaction.test.js',
-  //     './sample/diff-documents.test.js',
-  //     './sample/measurement.test.js',
-  //     './sample/offline.test.js',
-  //     './sample/flipbook.test.js',
-  //     './sample/custom-save.test.js',
-  //     './sample/display-points.test.js',
-  //     './sample/edit.test.js',
-  //     './sample/preprocess.test.js',
-  //     './sample/PDF-diff.test.js',
-  //     './sample/snap-to-nearest.test.js',
-  //     './sample/deck.test.js',
-  //     './sample/add-image.test.js',
-  //     './sample/bookmark.test.js',
-  //     './sample/content-replacer.test.js',
-  //     './sample/digital-signature.test.js',
-  //     './sample/element-builder.test.js',
-  //     './sample/element-edit.test.js',
-  //     './sample/element-reader.test.js',
-  //     './sample/encryption.test.js',
-  //     './sample/XFDF-import.test.js',
-  //     './sample/interactive-forms.test.js',
-  //     './sample/logical-structure.test.js',
-  //     './sample/office-to-PDF.test.js',
-  //     './sample/PDFA.test.js',
-  //     './sample/PDF-draw.test.js',
-  //     './sample/PDF-layer.test.js',
-  //     './sample/PDF-page.test.js',
-  //     './sample/PDF-redaction.test.js',
-  //     './sample/PDF-rect.test.js',
-  //     './sample/SDF.test.js',
-  //     './sample/stamper.test.js',
-  //     './sample/text-extract.test.js',
-  //     './sample/text-search.test.js'
-  //   ]);
-  // });
+  describe.skip('Samples', function() {
+    importTests([
+      // './sample/legacy-viewing.test.js',
+      // './sample/users-and-permissions.test.js',
+      './sample/realtime-collaboration.test.js',
+      // './sample/custom-annotations.test.js',
+      // './sample/ui-customization.test.js',
+      // './sample/form-customization.test.js',
+      // './sample/i18n.test.js',
+      // './sample/page-operations.test.js',
+      // './sample/layer-separation.test.js',
+      // './sample/color-separation.test.js',
+      // './sample/XOD.test.js',
+      // './sample/text-position.test.js',
+      // './sample/redaction.test.js',
+      // './sample/diff-documents.test.js',
+      // './sample/measurement.test.js',
+      // './sample/offline.test.js',
+      // './sample/flipbook.test.js',
+      // './sample/custom-save.test.js',
+      // './sample/display-points.test.js',
+      // './sample/edit.test.js',
+      // './sample/preprocess.test.js',
+      // './sample/PDF-diff.test.js',
+      // './sample/snap-to-nearest.test.js',
+      // './sample/deck.test.js',
+      // './sample/add-image.test.js',
+      // './sample/bookmark.test.js',
+      // './sample/content-replacer.test.js',
+      // './sample/digital-signature.test.js',
+      // './sample/element-builder.test.js',
+      // './sample/element-edit.test.js',
+      // './sample/element-reader.test.js',
+      // './sample/encryption.test.js',
+      // './sample/XFDF-import.test.js',
+      // './sample/interactive-forms.test.js',
+      // './sample/logical-structure.test.js',
+      // './sample/office-to-PDF.test.js',
+      // './sample/PDFA.test.js',
+      // './sample/PDF-draw.test.js',
+      // './sample/PDF-layer.test.js',
+      // './sample/PDF-page.test.js',
+      // './sample/PDF-redaction.test.js',
+      // './sample/PDF-rect.test.js',
+      // './sample/SDF.test.js',
+      // './sample/stamper.test.js',
+      // './sample/text-extract.test.js',
+      // './sample/text-search.test.js'
+    ]);
+  });
 });
 
